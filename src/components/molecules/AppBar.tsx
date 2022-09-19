f13775fc3fd4871c7a6e2bf875ff851ddd5ea042
Andrea Scotto Di Minico <ascottodiminico@yahoo.it>
2022-09-19 18:48:55 +0200

    chore: app bar opens wallet modal

--- src/components/molecules/AppBar.tsx
+++ src/components/molecules/AppBar.tsx
@@ -3,43 +3,53 @@ import Toolbar from '@mui/material/Toolbar'
 import Typography from '@mui/material/Typography'
 import Button from '@mui/material/Button'
 import { Container } from '@mui/system'
-import { ui } from '../../constants/ui'
+import AddWalletsModal from './AddWalletsModal'
+import { useState } from 'react'
 
 const AppTopBar: React.FC = () => {
+	const [toggleModal, setToggleModal] = useState<boolean>(false)
+
 	const openModalHandler: () => void = () => {
-		alert('open dialog')
+		setToggleModal((prevState) => !prevState)
 	}
 
 	return (
-		<AppBar
-			position="static"
-			sx={{
-				background: '#fff',
-				color: 'grey.700',
-				boxShadow: '0 4px 48px rgba(0,0,0,.14)',
-			}}
-		>
-			<Container maxWidth="xl">
-				<Toolbar>
-					<Typography
-						variant="h6"
-						textAlign={'left'}
-						component="div"
-						sx={{ flexGrow: 1 }}
-					>
-						Wallet tracker
-					</Typography>
-					<Button
-						onClick={openModalHandler}
-						color="primary"
-						disableElevation
-						variant="contained"
-					>
-						Add ETH Wallets
-					</Button>
-				</Toolbar>
-			</Container>
-		</AppBar>
+		<>
+			<AddWalletsModal
+				toggleModal={toggleModal}
+				handleClose={openModalHandler}
+			/>
+
+			<AppBar
+				position="static"
+				sx={{
+					background: '#fff',
+					color: 'grey.700',
+					boxShadow: '0 4px 48px rgba(0,0,0,.14)',
+				}}
+			>
+				<Container maxWidth="xl">
+					<Toolbar>
+						<Typography
+							variant="h6"
+							textAlign="left"
+							component="h6"
+							sx={{ flexGrow: 1 }}
+						>
+							Wallet tracker
+						</Typography>
+						<Button
+							onClick={openModalHandler}
+							color="primary"
+							disableElevation
+							variant="contained"
+						>
+							Add ETH Wallets
+						</Button>
+					</Toolbar>
+				</Container>
+			</AppBar>
+		</>
 	)
 }
 
