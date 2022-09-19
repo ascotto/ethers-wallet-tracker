341ed3ff008abb448419205a7474da04f1e26726
Andrea Scotto Di Minico <ascottodiminico@yahoo.it>
2022-09-19 19:34:17 +0200

    chore: utility function to get balances

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,3 +2,24 @@ export interface WalletsInterface {
 	wallets: string[]
 	loadWallets: (params: object) => void
 }
+
+export type Tokens = {
+	name: string
+	address: string
+	decimals: number
+}
+
+export type TokenInfo = {
+	name: string
+	ticker: string
+	balance: string
+}
+
+export interface BalanceAddress {
+	(
+		token: string,
+		wallet: string,
+		provider: any,
+		decimals: number,
+	): Promise<TokenInfo>
+}
