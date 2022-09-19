e47689f9675b4947191709c06150f89bf8f10842
Andrea Scotto Di Minico <ascottodiminico@yahoo.it>
2022-09-19 22:27:34 +0200

    chore: wrapping global wallets provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
+import { GlobalWalletsProvider } from './store/wallets/wallets.provider'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
-	<React.StrictMode>
+	<GlobalWalletsProvider>
 		<App />
-	</React.StrictMode>,
+	</GlobalWalletsProvider>,
 )
