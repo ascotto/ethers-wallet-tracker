14f31b03b7adb9d2373ecf6c36e789269165d815
Andrea Scotto Di Minico <ascottodiminico@yahoo.it>
2022-09-19 18:53:06 +0200

    chore: added global wallets state to app

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,46 @@
-import logo from './logo.svg'
 import './App.css'
+import { Chip, Container, Divider, Grid, Typography } from '@mui/material'
+import { ui } from './constants/ui'
+import AppTopBar from './components/molecules/AppBar'
+import { useContext, useEffect } from 'react'
+import { getWallets } from './utils/storage'
+import { GlobalWalletsStore } from './store/wallets/wallets.store'
 
-function App() {
+const App: React.FC = () => {
+	const { wallets, loadWallets } = useContext(GlobalWalletsStore)
+
+	useEffect(() => {
+		if (wallets.length === 0) {
+			getWalletsHandler()
+		}
+	}, [])
+
+	const getWalletsHandler = async () => {
+		const ethWallets = await getWallets()
+		loadWallets(ethWallets)
+	}
+
 	return (
 		<div className="App">
-			<header className="App-header">
-				<img src={logo} className="App-logo" alt="logo" />
-				<p>
-					Edit <code>src/App.tsx</code> and save to reload.
-				</p>
-				<a
-					className="App-link"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-			</header>
+			<AppTopBar />
+			<Container maxWidth={ui.xl}>
+				<Grid container sx={{ mt: 3 }} spacing={2}>
+					<Grid item xs={3}>
+						<Typography variant="h6" gutterBottom component="h6">
+							ETH Wallets
+						</Typography>
+						{wallets.length > 0 ? (
+							wallets.map((wallet, i) => <Chip key={i} label={wallet} />)
+						) : (
+							<p>No wallets</p>
+						)}
+					</Grid>
+					<Divider orientation="vertical" flexItem />
+					<Grid item xs>
+						<Typography variant="h6" gutterBottom component="h6"></Typography>
+					</Grid>
+				</Grid>
+			</Container>
 		</div>
 	)
 }
