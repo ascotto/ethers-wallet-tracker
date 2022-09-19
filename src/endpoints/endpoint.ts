69d5d4059015265b1677e9b9e466d0650c387d02
Andrea Scotto Di Minico <ascottodiminico@yahoo.it>
2022-09-19 23:52:07 +0200

    chore: added HTTP endpoint

--- src/endpoints/endpoint.ts
+++ src/endpoints/endpoint.ts
@@ -1,3 +1,4 @@
 const WSS_API = 'wss://mainnet.infura.io/ws/v3/50c862702d5847a387026d5464429c77'
+const HTTP_API = 'https://mainnet.infura.io/v3/50c862702d5847a387026d5464429c77'
 
-export default WSS_API
+export { WSS_API, HTTP_API }
