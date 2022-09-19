fd4a83e60863d8b2b601a9fe351afacbec17da18
Andrea Scotto Di Minico <ascottodiminico@yahoo.it>
2022-09-19 19:05:48 +0200

    fix: initial value of global wallet state

--- src/store/wallets/wallets.store.tsx
+++ src/store/wallets/wallets.store.tsx
@@ -1,5 +1,6 @@
 import { createContext } from 'react'
 import { WalletsInterface } from '../../types'
+import { WALLETS_STATE } from './wallets.state'
 
-export const GlobalWalletsStore = createContext<WalletsInterface>(null!)
+export const GlobalWalletsStore = createContext<WalletsInterface>(WALLETS_STATE)
 GlobalWalletsStore.displayName = 'GlobalWalletsStore'
